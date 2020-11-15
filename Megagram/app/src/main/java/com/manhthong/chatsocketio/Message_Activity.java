3b8759e6065c8464ec7970dcd959f5445a14091a
ManhThong <manhthong1105@gmail.com>
2020-11-15 17:32:31 +0700

    them giao dien nhan tin 2 nguoi

--- Megagram/app/src/main/java/com/manhthong/chatsocketio/Message_Activity.java
+++ Megagram/app/src/main/java/com/manhthong/chatsocketio/Message_Activity.java
@@ -6,10 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.manhthong.chatsocketio.Adapter.MessageAdapter;
+import com.manhthong.chatsocketio.Model.MessageFormat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Message_Activity extends AppCompatActivity {
     ImageButton btnBack, btnInfo;
+    private EditText edt_send;
+    private ImageButton btn_send;
+
+    public static final String TAG  = "Message_Activity";
+    public static String uniqueId;
+
+    private String Username;
+
+    private Boolean hasConnection = false;
+
+    private ListView messageListView;
+    private MessageAdapter messageAdapter;
+
+    private Thread thread2;
+    private boolean startTyping = false;
+    private int time = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +57,22 @@ public class Message_Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        edt_send = findViewById(R.id.edt_send);
+        btn_send = findViewById(R.id.btn_send);
+        messageListView = findViewById(R.id.messageListView);
+
+        List<MessageFormat> messageFormatList = new ArrayList<>();
+        messageFormatList.add(new MessageFormat("00001", "Hữu Lộc", "Chào cậu"));
+        messageFormatList.add(new MessageFormat("00002", "Thông", "Chào cậu :))"));
+        messageFormatList.add(new MessageFormat("00001", "Hữu Lộc", "Cậu ăn cơm chưa????"));
+        messageFormatList.add(new MessageFormat("00001", "Hữu Lộc", "Có ăn cơm với canh không????"));
+        messageFormatList.add(new MessageFormat("00002", "Thông", "Cậu vui tánh quá :))"));
+        messageFormatList.add(new MessageFormat("00001", "Hữu Lộc", "Cậu quá khen hihi :))"));
+        messageFormatList.add(new MessageFormat("00002", "Thông", ":) "));
+        messageFormatList.add(new MessageFormat("00001", "Hữu Lộc", "Cậu ăn cơm chưa????"));
+        messageFormatList.add(new MessageFormat("00001", "Hữu Lộc", "Có ăn cơm với canh không????"));
+        messageFormatList.add(new MessageFormat("00002", "Thông", "Cậu vui tánh quá :))"));
+        messageAdapter = new MessageAdapter(this, R.layout.item_message, messageFormatList);
+        messageListView.setAdapter(messageAdapter);
     }
 }
