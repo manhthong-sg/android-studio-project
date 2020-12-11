bf95bc7a5a1ccff5214e10c3424714537d0a89a4
ManhThong <manhthong1105@gmail.com>
2020-12-11 14:02:46 +0700

    add mongoose to server

--- megagramServer/server.js
+++ megagramServer/server.js
@@ -3,31 +3,43 @@ var app        = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-
+const mongoose = require('mongoose');
 
 app.get('/',function(req,res){
     res.send("Welcome to my socket");
 });
+ // connect mongoose
+ mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useCreateIndex', true);
+mongoose.connect("mongodb://localhost:27017/megagramDB");
 
-var mess=[];
-  io.sockets.on('connection', function(socket){
+//tao Schema
+const userSchema=new mongoose.Schema({
+  name: String,
+  age: Number
+})
+//tao model
+const user=mongoose.model('user', userSchema);
+// add gia tri
+user.create([
+  {name: "Thong Bui", age: 21},
+  {name: "Thai Bui", age: 19},
+  {name: "Thui Bui", age: 20}
+])
 
-    console.log('User Conncetion');
+  io.on('connection', function(socket){
+
+    console.log('User Connection '+socket.id);
     
-      socket.on('client-gui-tn',function(msg){
-          console.log(msg);
-          mess.push(msg);
-          io.sockets.emit('onMessage',{noidung:mess})
+      socket.on('client-gui-tn',function(message){
+          console.log(message);
+          io.sockets.emit('onMessage', {noidung: message})
       })
-
-
-    // socket.emit('user-connect' , {socketID: socket.id});
-    // socket.on('client-gui-tn', function(msg){
-    //   console.log("Message " + msg+socket.id);
-      
-    //  //io.emit('server-gui-tn', {noidung: msg,socket:socket.id});
-    //   io.to(socket.id).emit('server-gui-tn',{noidung:msg,socket:socket.id})
-    // });
+      // Check disconnected
+    socket.on('disconnect', reason => {
+      console.log(`${ new Date().toLocaleTimeString() }: ${ socket.id } has disconnected because ${ reason }`);
+  });
   });
 http.listen(port, function () {
   console.log('Server listening at port %d', port);
