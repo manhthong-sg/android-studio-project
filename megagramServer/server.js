e5eb3e30903c3c559747627c1c6beb1fdf19da0c
ManhThong <manhthong1105@gmail.com>
2020-12-17 10:11:13 +0700

    update server for login feature

--- megagramServer/server.js
+++ megagramServer/server.js
@@ -14,27 +14,43 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.set('useCreateIndex', true);
 mongoose.connect("mongodb://localhost:27017/megagramDB");
 
-//tao Schema
-const userSchema=new mongoose.Schema({
-  name: String,
-  age: Number
-})
-//tao model
-const user=mongoose.model('user', userSchema);
-// add gia tri
-user.create([
-  {name: "Thong Bui", age: 21},
-  {name: "Thai Bui", age: 19},
-  {name: "Thui Bui", age: 20}
-])
+// //tao Schema
+// const userSchema=new mongoose.Schema({
+//   name: String,
+//   age: Number
+// })
+// //tao model
+// const user=mongoose.model('user', userSchema);
+// // add gia tri
+// user.create([
+//   {name: "Thong Bui", age: 21},
+//   {name: "Thai Bui", age: 19},
+//   {name: "Thui Bui", age: 20}
+// ])
 
+//const user_info=mongoose.get('user_info');
+
+//check user connect socketIO
   io.on('connection', function(socket){
-
+    
     console.log('User Connection '+socket.id);
     
       socket.on('client-gui-tn',function(message){
           console.log(message);
           io.sockets.emit('onMessage', {noidung: message})
+      })
+      socket.on('client-login', (user_login)=>{
+
+          var username=user_login["username"];
+          var password = user_login['password'];
+          console.log(username+" "+password);
+          if(username=="0796705768" && password=="123"){
+              console.log("tdn va pass dung roi!");
+              socket.emit('login_status', {nd: true} )
+          }
+          else{
+            socket.emit('login_status', {nd: false} )
+          }
       })
       // Check disconnected
     socket.on('disconnect', reason => {
