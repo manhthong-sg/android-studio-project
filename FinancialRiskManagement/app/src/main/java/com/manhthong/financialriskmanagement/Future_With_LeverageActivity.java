bd055a1c70e788e89af1a46abdee57db6b1dfe77
ManhThong <manhthong1105@gmail.com>
2021-06-28 17:48:31 +0700

    add Tinh lenh SHORT feature

--- FinancialRiskManagement/app/src/main/java/com/manhthong/financialriskmanagement/Future_With_LeverageActivity.java
+++ FinancialRiskManagement/app/src/main/java/com/manhthong/financialriskmanagement/Future_With_LeverageActivity.java
@@ -3,12 +3,16 @@ package com.manhthong.financialriskmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,9 +26,11 @@ public class Future_With_LeverageActivity extends AppCompatActivity {
     EditText target;
     EditText donBay;
     TextView tv_spot;
+    Switch sw_short;
     Button btn_Tinh;
 
     boolean tongVon_flag=false;
+    Integer long_short=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +42,7 @@ public class Future_With_LeverageActivity extends AppCompatActivity {
         target=findViewById(R.id.edt_target);
         donBay=findViewById(R.id.edt_leverage);
         tv_spot=findViewById(R.id.tv_Spot);
+        sw_short=findViewById(R.id.sw_short);
         btn_Tinh=findViewById(R.id.btn_Tinh);
 
 
@@ -56,6 +63,23 @@ public class Future_With_LeverageActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+        sw_short.setChecked(false);
+        //set sự kiện cho switch short
+        sw_short.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    long_short=0;
+                    btn_Tinh.setSelected(true);
+                    btn_Tinh.setText("Tính lệnh SHORT");
+                }
+                else {
+                    long_short=1;
+                    btn_Tinh.setSelected(false);
+                    btn_Tinh.setText("Tính lệnh LONG");
+                }
             }
         });
 
@@ -80,7 +104,7 @@ public class Future_With_LeverageActivity extends AppCompatActivity {
                 Double DonBay=Double.parseDouble(donBay.getText().toString());
                 Double Target=Double.parseDouble(target.getText().toString());
 
-                if(checkValuedation()){
+                if(long_short==1){
                     Double soTienBoRa=TongVon*RuiRo*1/100*Entry/(DonBay*(Entry-Stoploss));
 
                     Double soCoinCanMua=soTienBoRa/Entry;
@@ -108,8 +132,34 @@ public class Future_With_LeverageActivity extends AppCompatActivity {
 
                     startActivity(intentResult);
                 }
-                else{
-                    Toast.makeText(Future_With_LeverageActivity.this, "Please fill up all fields!", Toast.LENGTH_LONG).show();
+                if(long_short==0){
+                    //Toast.makeText(Future_With_LeverageActivity.this, "Bạn muốn SHORT?", Toast.LENGTH_SHORT).show();
+                    Double soTienBoRa=TongVon*RuiRo*1/100*Entry/(DonBay*(Stoploss-Entry));
+
+                    Double soCoinCanMua=soTienBoRa/Entry;
+
+                    Double LoiNhuan=DonBay*(1-Target/Entry)*100;
+
+                    Double tiLeRR=(soTienBoRa*LoiNhuan/100)/(TongVon*RuiRo/100);
+
+                    soCoinCanMua=(double)Math.round(soCoinCanMua*10000)/10000;
+                    soTienBoRa=(double)Math.round(soTienBoRa*100)/100;
+                    LoiNhuan=(double)Math.round(LoiNhuan*100)/100;
+                    tiLeRR=(double)Math.round(tiLeRR*100)/100;
+
+                    Intent intentResult = new Intent(Future_With_LeverageActivity.this, Result_Future_With_LeverageActivity.class);
+                    intentResult.putExtra("tongVon",tongVon.getText().toString());
+                    intentResult.putExtra("ruiRo", ruiRo.getText().toString());
+                    intentResult.putExtra("stoploss", stoploss.getText().toString());
+                    intentResult.putExtra("entry", entry.getText().toString());
+                    intentResult.putExtra("target", target.getText().toString());
+                    intentResult.putExtra("donBay", donBay.getText().toString());
+                    intentResult.putExtra("SoCoiCanMua", soCoinCanMua.toString());
+                    intentResult.putExtra("SoTienBoRa", soTienBoRa.toString());
+                    intentResult.putExtra("LoiNhuan", LoiNhuan.toString());
+                    intentResult.putExtra("tiLeRR", tiLeRR.toString());
+
+                    startActivity(intentResult);
                 }
             }
         });
